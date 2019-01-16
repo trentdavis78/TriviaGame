83dee476a73daf838a67e11c43a2ff16d0b6c7c8
Trent Davis <trendavisinc@gmail.com>
2019-01-16 01:03:53 -0600

    Added high score sorting functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,18 @@
 $(document).ready(function() {   
   // Initialize Firebase
-  var config = {
+var config = {
     apiKey: "AIzaSyARgHGjUoNIXG9BTX096kVLoT5Md_HmKQo",
     authDomain: "trivia-game-ec53a.firebaseapp.com",
     databaseURL: "https://trivia-game-ec53a.firebaseio.com",
     projectId: "trivia-game-ec53a",
     storageBucket: "",
     messagingSenderId: "632296056982"
-  };
+};
   firebase.initializeApp(config);
 //  game variables 
 var intervalId;
-var time = 4;
+var origTime = 4;
+var time;
 var clockRunning = false;
 var qNum = 0;
 var wins = 0;
@@ -22,23 +23,30 @@ var questionLength = 0;
 var category;
 var score = 0;
 var categorySelected = 0;
+var streak = 0;
+// high score variables 
 var leaderScore0;
 var leaderScore1;
 var leaderScore2;
-var hsPath;
+var leaderName0;
+var leaderName1;
+var leaderName2;
+var scoreArr = [];
 
-    // onclick event to hide the splash screen, unhide game content and start the game
-    $(".categoryBtn").on("click", function(e){
-        $("#splash").addClass("hidden");
-        $("#leaderboard").addClass("hidden");
-        $("#gameContent").removeClass("hidden");   
-        $("#"+e.target.id).removeClass("btn-theme");  
-        $("#"+e.target.id).addClass("btn-secondary");  
-        $("#"+e.target.id).attr("disabled", true);      
-        category = e.target.value;
-        categorySelected++;
-        startRound();
-    });
+
+// onclick event to hide the splash screen, unhide game content and start the game
+$(".categoryBtn").on("click", function(e){
+    $("#splash").addClass("hidden");
+    $("#leaderboard").addClass("hidden");
+    $("#gameContent").removeClass("hidden");   
+    $("#"+e.target.id).removeClass("btn-theme");  
+    $("#"+e.target.id).addClass("btn-secondary");  
+    $("#"+e.target.id).attr("disabled", true);      
+    category = e.target.value;
+    categorySelected++;
+    time = origTime;
+    startRound();
+});
    
     //change bg color when radio is selected
     $('#answerRow input:radio').change(function() {
@@ -72,6 +80,7 @@ var hsPath;
             checkResponse();          
         }
     }
+    // initial game function 
     function startRound(){    
         // set questionLength variable
         firebase.database().ref().child(category).on('value', function(snap){
@@ -81,26 +90,27 @@ var hsPath;
         $("#timer").text(time);
         startTimer();
     }
+    // update HTML with trivia question and answers
     function askQuestion() {
-
-                // set reference objects
-                var dbRefObject = firebase.database().ref().child(category).child('q'+qNum);
-                var questionRef = dbRefObject.child('question');
-                var choicesRef = dbRefObject.child('choices');
-                var answerRef = dbRefObject.child('answer');                
-                // write answers to the choice radio button spans
-                choicesRef.on('child_added', snap => {
-                    $("#answer-"+snap.key).text(snap.val());
-                });
-                // write the question to the jumbotron
-                questionRef.on('value', snap => $("#question").text(snap.val()));
-                // set the answer to the answer variable 
-                answerRef.on('value', function(snap){
-                    answer = snap.val();           
+        // set reference objects
+        var dbRefObject = firebase.database().ref().child(category).child('q'+qNum);
+        var questionRef = dbRefObject.child('question');
+        var choicesRef = dbRefObject.child('choices');
+        var answerRef = dbRefObject.child('answer');                
+        // write answers to the choice radio button spans
+        choicesRef.on('child_added', snap => {
+            $("#answer-"+snap.key).text(snap.val());
+        });
+        // write the question to the jumbotron
+        questionRef.on('value', snap => $("#question").text(snap.val()));
+        // set the answer to the answer variable 
+        answerRef.on('value', function(snap){
+            answer = snap.val();           
                 
         });
         
     }
+    // check answer selected and then fire resetQuestion function
     function checkResponse(){
         var response = $("input[name='answers']:checked").val();
         if(response){
@@ -116,10 +126,7 @@ var hsPath;
         } else {
             $("#answer-"+answer).addClass("correct unanswered");  
             unanswered++;
-        }
-              
-        // console.log(wins, losses, unanswered, score);
-        
+        }        
         setTimeout(resetQuestion, 1500);
     }
     function resetQuestion() {
@@ -128,7 +135,7 @@ var hsPath;
         $('.answers').children("span").removeClass("unanswered");
         $('.answers').children("input[type=radio]").prop("checked", false);
         $('div.highlight').removeClass('highlight');        
-        time = 4;        
+        time = origTime;        
         qNum++;
         if(categorySelected < 4){
             if(qNum < questionLength){            
@@ -141,6 +148,7 @@ var hsPath;
         }      
         
     }
+    // function fired at the end of each category
     function endRound() {
        $("#gameContent").addClass("hidden");
        $("#endRound").removeClass("hidden");   
@@ -149,25 +157,20 @@ var hsPath;
        $("#unanswered").text(unanswered);     
        $("#score").text(score);   
     }
+    // function fired when all categories have been completed
     function endGame() {
         $("#gameContent").addClass("hidden");
         $("#endGame").removeClass("hidden");   
-        $("#finalScore").text(score);   
-        console.log(score);
-        
+        $("#finalScore").text(score);       
+        // check to see if a new high score has been reached           
         if(score > parseInt(leaderScore2)) {
-            $("#newHighScore").removeClass("hidden"); 
-            if(score > parseInt(leaderScore2) && score < parseInt(leaderScore1) ) {
-                hsPath = "p2";
-            } else if(score > parseInt(leaderScore1) && score < parseInt(leaderScore0)) {
-                hsPath = "p1";
-            } else {
-                hsPath = "p0";
-            }
+            $("#newHighScore").removeClass("hidden");             
+            sortHighScore()
         }
     }
+    // function to reset game for new category 
     function restartGame() {
-        time = 4;        
+        time = origTime;        
         qNum = 0;
         wins = 0;
         losses = 0;
@@ -178,63 +181,99 @@ var hsPath;
         $("#endRound").addClass("hidden");   
         $("#newHighScore").addClass("hidden"); 
     }
+    // click event for Next Category button
     $("#nextCategory").on("click", function(){
         restartGame();
     });
+    // function to Try Again and start over
     function startOver() {
         score = 0;        
         categorySelected = 0;        
         $("#endGame").addClass("hidden");  
         restartGame();
     }
+    // click event for Try Again button
     $("#startOver").on("click", function(){
         $(".categoryBtn").attr("disabled", false);
         $(".categoryBtn").removeClass("btn-secondary");
         $(".categoryBtn").addClass("btn-theme");       
         startOver();
     });
+    // pull data from Firebase, update the HTML, and set high score variables
     function loadLeaderBoard(){         
         var leaderRef = firebase.database().ref().child("highscores");
         leaderRef.child("p0").child('name').on('value', snap => {
-            $("#p0").text(snap.val());            
+            $("#p0").text(snap.val());     
+            leaderName0 = snap.val();       
         }); 
         leaderRef.child("p0").child('score').on('value', snap => {
             $("#p0score").text(snap.val());  
-            leaderScore0 = snap.val();            
+            leaderScore0 =  parseInt(snap.val());            
         });    
         leaderRef.child("p1").child('name').on('value', snap => {
-            $("#p1").text(snap.val());            
+            $("#p1").text(snap.val());    
+            leaderName1 = snap.val();          
         }); 
         leaderRef.child("p1").child('score').on('value', snap => {
             $("#p1score").text(snap.val());  
-            leaderScore1 = snap.val();  
+            leaderScore1 = parseInt(snap.val());  
         }); 
         leaderRef.child("p2").child('name').on('value', snap => {
-            $("#p2").text(snap.val());            
+            $("#p2").text(snap.val());   
+            leaderName2 = snap.val();           
         }); 
         leaderRef.child("p2").child('score').on('value', snap => {
             $("#p2score").text(snap.val());  
-            leaderScore2 = snap.val();  
+            leaderScore2 = parseInt(snap.val());  
         });
     }
-    loadLeaderBoard();
-
-    $("#addHighScore").on('click', function(){
+    loadLeaderBoard();   
+    // sort high scores to update Firebase
+    function sortHighScore(){
+        scoreArr = [["hsName",score], [leaderName0, leaderScore0], [leaderName1, leaderScore1], [leaderName2, leaderScore2]];
+        scoreArr.sort(function compareSecondColumn(a, b) {
+            if (a[1] === b[1]) {
+                return 0;
+            }
+            else {
+                return (a[1] < b[1]) ? -1 : 1;
+            }
+        });       
+    }
+    // click event for Add button on New Hire Score div
+     $("#addHighScore").on('click', function(){
+         // get input value from text field
         var hsName = $("#newHighScoreName").val();        
-        var hsObj = {
-            name : hsName,
-            score: score
+        // replace placeholder for hsName in scoreArr with text field value
+        for(var i=0; i < scoreArr.length; i++) {            
+            scoreArr[i][0] = scoreArr[i][0].replace("hsName", hsName);
+           }       
+        // create data objs for Firebase
+        var hsObj0 = {
+            name : scoreArr[3][0],
+            score: scoreArr[3][1]
         }
-        var path = hsPath;
-        updateHighScore(path,hsObj);
+        var hsObj1 = {
+            name : scoreArr[2][0],
+            score: scoreArr[2][1]
+        }
+        var hsObj2 = {
+            name : scoreArr[1][0],
+            score: scoreArr[1][1]
+        }  
+        // fire update score function for each newly sorted score           
+        updateHighScore("p0",hsObj0);
+        updateHighScore("p1",hsObj1);
+        updateHighScore("p2",hsObj2);
+        // reset CSS
         $("#newHighScoreName").val("");
         $("#newHighScore").addClass("hidden"); 
         $("#leaderboard").removeClass("hidden");
     });
+    //update Firebase high scores
     function updateHighScore(path, data) {
         var leaderRef = firebase.database().ref().child("highscores");
         leaderRef.child(path).update(data);
     }
-    
   });
   
